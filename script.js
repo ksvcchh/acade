7818dac04e448f5bed369146e417d17ddc800bf2
Mikita Kasevich <m.kasevich.801@studms.ug.edu.pl>
2024-09-16 00:51:11 +0200

    adds new button that should make books appear in new tabs

--- script.js
+++ script.js
@@ -26,13 +26,43 @@ function handleCopyYesNo() {
     copy(YesNo);
 };
 
-function checkBooks() {
-    function currBooks() {
-        let BooksParser = document.querySelectorAll('.item');
-        let listOfDownloadedBooks = [...BooksParser].map((elem) =>
-            elem.innerHTML.split("\"")[1].split("\/")[2]);
-        console.log(listOfDownloadedBooks);
+
+window.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('newPageStatus');
+    const checkBox = document.getElementById('newPage');
+
+    function touchNewPageButton() {
+        if (button.innerText == "В той же вкладке") {
+            button.innerText = "В новой вкладке";
+            localStorage.setItem('newPageStatus', 'В новой вкладке');
+            localStorage.setItem('newPage', 'true');
+        }
+        else {
+            button.innerText = "В той же вкладке";
+            localStorage.setItem('newPageStatus', 'В той же вкладке');
+            localStorage.setItem('newPage', 'false');
+        }
     }
 
-    shouldBeBooks();
-}
+    const savedStatus = localStorage.getItem('newPageStatus');
+    if (savedStatus) {
+        button.innerText = savedStatus;
+    }
+
+    button.onclick = touchNewPageButton;
+
+    const links = document.querySelectorAll('.item a');
+
+    if (localStorage.getItem('newPage') === 'true') {
+        checkBox.checked = true;
+        addTargetBlank();
+    }
+
+    function addTargetBlank() {
+        links.forEach(link => link.setAttribute('target', '_blank'));
+    }
+
+    function removeTargetBlank() {
+        links.forEach(link => link.removeAttribute('target'));
+    }
+});
