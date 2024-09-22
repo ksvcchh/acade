dcc4231839cbbce85e040338de14cb43b6e30391
Mikita Kasevich <m.kasevich.801@studms.ug.edu.pl>
2024-09-22 16:03:45 +0200

    added new feature: insta-opening books

--- script.js
+++ script.js
@@ -65,4 +65,180 @@ window.addEventListener('DOMContentLoaded', function() {
     function removeTargetBlank() {
         links.forEach(link => link.removeAttribute('target'));
     }
+
+    function isValidBook(nodeList, book) {
+        
+        if (book == "Алгебра 9 класс. Л. Г. Петерсон. Часть 1 и 2") {
+            const part = prompt("Какая часть?")
+            if (part == 1) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра 9 класс. Л. Г. Петерсон. Часть 1.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == 2) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра 9 класс. Л. Г. Петерсон. Часть 2.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        if (book == "Алгебра. 8 класс. Петерсон.") {
+            const part = prompt("Какая часть?")
+            if (part == 1) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра. 8 класс. Петерсон. 1 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == 2) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра. 8 класс. Петерсон. 2 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == 3) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра. 8 класс. Петерсон. 3 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+        }
+
+        if (book == "Математика 7-ой класс. Петерсон") {
+            const part = prompt("Какая часть?")
+            if (part == 1) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Математика 7-ой класс. Петерсон, 1 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == 2) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Математика 7-ой класс. Петерсон, 2 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == 3) {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Математика 7-ой класс. Петерсон, 3 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+        }
+
+        if (book == "Алгебра 8 класс. Углубленный. Мордкович") {
+            const part = prompt("Какая часть? ('1', если задания, '2' если теория)")
+            if (part == "1") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра 8 класс. Углубленный. Мордкович 2 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == "2") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра 8 класс. Углубленный. Мордкович 1 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        if (book == "Алгебра 9-ый класс. Мордкович") {
+            const part = prompt("Какая часть? ('1', если задания, '2' если теория)")
+            if (part == "1") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра 9-ый класс. Мордкович 2 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            if (part == "2") {
+                for (const elem of nodeList) {
+                    if (elem.attributes.href.nodeValue == `./учебники/Алгебра 9-ый класс. Мордкович 1 часть.pdf`) {
+                        elem.click();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        if (book == "Сборник задач по алгебре: учебное пособие для 8-9 кл. с углубленным изучением математики - Галицкий М.Л., Гольдман А.М., Звавич Л.И.") {
+            for (const elem of nodeList) {
+                if (elem.attributes.href.nodeValue == `./учебники/Сборник задач по алгебре для 8-9 углубленный Галицкий.pdf`) {
+                    elem.click();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        for (const elem of nodeList) {
+            if (elem.attributes.href.nodeValue == `./учебники/${book}.pdf`) {
+                elem.click();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    this.document.addEventListener("paste", function(event) {
+        const book = event.clipboardData.getData('text/plain').trim();
+        bookSearch(event, book);
+    });
+
+    this.document.addEventListener("dragover", function(event) {
+        event.preventDefault();
+    });
+
+    this.document.addEventListener("drop", function(event) {
+        event.preventDefault();
+        const book = event.dataTransfer.getData('text/plain').trim();
+        bookSearch(event, book);
+    });
+
+    function bookSearch(event, book) {
+        const nodeList = Array.from(document.querySelectorAll("div a"));
+        const notification = document.getElementById('notification');
+        // const book = event.clipboardData.getData('text/plain').trim();
+
+        if (!isValidBook(nodeList, book)) {
+            notification.classList.remove('fade-out');
+            notification.classList.add('show');
+        
+            setTimeout(function() {
+                notification.classList.add('fade-out');
+            }, 2000);
+        
+    
+            setTimeout(function() {
+                notification.classList.remove('show');
+            }, 2500);
+        }    
+    }
 });
